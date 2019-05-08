bee92b2be3e02ba02ac03d2367f1be12882884fb
David Laubenstein <david.laubenstein@posteo.de>
2019-05-08 13:28:09 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "MarqueeLabel"
+    name: "MarqueeLabel",
+    // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    products: [
+        .library(name: "MarqueeLabel", targets: ["MarqueeLabel"])
+    ],
+    targets: [
+        .target(
+            name: "MarqueeLabel",
+            path: "MarqueeLabel"
+        )
+    ]
 )
